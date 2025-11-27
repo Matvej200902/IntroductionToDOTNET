babd99bf19e046a1edee97de1a08922e9623cf61
tabta <tabta@TABTABUR>
2025-11-27 21:37:26 +0900

    Arrays d2

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿//#define ARRAYS_1
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Arrays {
     internal class Program {
+        const string delimiter = "\n-----------------------------------------------------------------------\n";
         static void Main(string[] args)
         {
-            const string delimiter = "\n-----------------------------------------------------------------------\n";
+#if ARRAYS_1
             int[] arr = { 3, 5, 8, 13, 21 };
             for (int i = 0; i < arr.Length; i++) {
-                Console.WriteLine(arr[i] + "\t");
+                Console.Write(arr[i] + "\t");
             }
             Console.WriteLine();
 
@@ -44,6 +47,55 @@ namespace Arrays {
             Console.WriteLine("Минимальное: " + (min.HasValue ? min.Value.ToString() : ""));
             Console.WriteLine("Максимальное: " + (max.HasValue ? max.Value.ToString() : ""));
             Console.WriteLine(delimiter);
-        }
+#endif
+
+            Random rand = new Random(0);
+            Console.Write("Введите количество строк: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество элементов строки: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[rows, cols];
+            Console.WriteLine($"Количество измерений массива: {arr.Rank}");
+
+
+
+            int sum = 0;
+            for (int i = 0; i < arr.GetLength(0); i++) {
+                for (int j = 0; j < arr.GetLength(1); j++) {
+                    arr[i, j] = rand.Next(100);
+                    sum += arr[i, j];
+                }
+            }
+
+            for (int i = 0; i < arr.GetLength(0); i++) {
+                for (int j = 0; j < arr.GetLength(1); j++) {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+			int min = int.MaxValue;
+			int max = int.MinValue;
+
+			for (int i = 0; i < arr.GetLength(0); i++) {
+				for (int j = 0; j < arr.GetLength(1); j++) {
+					int value = arr[i, j];
+
+					sum += value;
+					if (value < min) min = value;
+					if (value > max) max = value;
+				}
+			}
+			Console.WriteLine();
+
+            Console.WriteLine($"Сумма элементов массива: {sum}");
+            Console.WriteLine($"Среднее арифмитическое: {(double)sum / arr.Length}");
+
+            Console.WriteLine(delimiter);
+            Console.WriteLine($"Сумма элементов массива: {arr.Cast<int>().Sum()}");
+			Console.WriteLine($"Минимальное значение: {min}");
+			Console.WriteLine($"Максимальное значение: {max}");
+		}
     }
 }
